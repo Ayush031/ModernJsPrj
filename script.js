7db91ba0de2f720de4bb8c6bc770edd32fb7ca7b
Ayush <ayushkansal303@gmail.com>
2024-01-05 03:20:02 +0530

    sticky-pointer done

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),// the top or head element i.e wrapper
     smooth: true
 });
+
+function mouseFollower() {
+    window.addEventListener('mousemove', function (e) {
+        const circle = document.querySelector('#sticky-pointer');
+        circle.style.transform = `translate(${e.clientX-4}px,${e.clientY-4}px)`
+    });
+}
+mouseFollower();
