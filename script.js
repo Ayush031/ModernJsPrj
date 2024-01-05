7db17454a7d71771953e39ff11a334c975c23c89
Ayush <ayushkansal303@gmail.com>
2024-01-05 18:32:30 +0530

    first page animation done

--- script.js
+++ script.js
@@ -6,7 +6,25 @@ const scroll = new LocomotiveScroll({
 function mouseFollower() {
     window.addEventListener('mousemove', function (e) {
         const circle = document.querySelector('#sticky-pointer');
-        circle.style.transform = `translate(${e.clientX-4}px,${e.clientY-4}px)`
+        circle.style.transform = `translate(${e.clientX - 4}px,${e.clientY - 4}px)`
     });
 }
+
+function firstPageanim() {
+    const tl = gsap.timeline();
+
+    tl.from('#nav', {
+        duration: 1.5,
+        y: -10,
+        opacity: 0,
+        ease: Expo.easeInOut
+    }).to('.boundingElem', {
+        y: 0,
+        ease: Expo.easeInOut,
+        duration: 1.25,
+        stagger: .2
+    });
+}
+
 mouseFollower();
+firstPageanim();
